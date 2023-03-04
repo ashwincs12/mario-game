b6d3398cff381365845307a2d3fab4da7f9e4e1e
subin1511 <subinkrishnana005@gmail.com>
2023-03-04 14:30:07 +0530

    playermovement

--- index.js
+++ index.js
@@ -25,13 +25,77 @@ class Player{
     update() {
         this.position.y += this.velocity.y
         this.draw()
+        this.position.x +=this.velocity.x
     }
 }
 
 const player = new Player()
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+}
 player.draw()
 
 function animate(){
     requestAnimationFrame(animate)
     player.update()
-}
+
+    if (keys.right.pressed) {
+        player.velocity.x = 5
+    }else if (keys.left.pressed) {
+        player.velocity.x = -5
+    } else player.velocity.x = 0
+} 
+
+addEventListener('keydown',  ({ keyCode }) => {
+    console.log(keyCode)
+    switch (keyCode) {
+        case 65:
+            console.log('left')
+            keys.left.pressed = true
+            break
+        
+         case 83:
+            console.log('down')
+            break
+        
+        case 68:
+            console.log('right')
+            keys.right.pressed = true
+            break
+        case 87:
+            console.log('up')
+            player.velocity.y  -= 20
+            break
+    }
+
+})
+
+addEventListener('keyup',  ({ keyCode }) => {
+    console.log(keyCode)
+    switch (keyCode) {
+        case 65:
+            console.log('left')
+            keys.left.pressed = false
+            break
+        
+         case 83:
+            console.log('down')
+            break
+        
+        case 68:
+            console.log('right')
+            keys.right.pressed = false
+            break
+        case 87:
+            console.log('up')
+            player.velocity.y  -= 20
+            break
+    }
+
+    console.log(keys.right.pressed)
+})
