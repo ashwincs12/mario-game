9def930d2fb353443a921a2317cde121342817d8
abi1230 <abhiramsuresh55@gmail.com>
2023-03-05 13:56:09 +0530

    latest

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const c = canvas.getContext('2d')
 canvas.width = 1026
 canvas.height = 576
 
-const gravity = 3
+const gravity = 2
 class Player{
     constructor(){
+        this.speed=5
         this.position = {
             x:100,
             y:100
@@ -20,7 +21,7 @@ class Player{
     }
 
     draw(){
-        c.fillStyle = 'blue'
+        c.fillStyle = 'red'
         c.fillRect(this.position.x,this.position.y,this.height,this.width,this.velocity)
     }
     update() {
@@ -30,8 +31,7 @@ class Player{
 
         if(this.position.y+this.height+this.velocity.y <= canvas.height)
             this.velocity.y += gravity
-        else 
-            this.velocity.y = 0
+
     }
 }
 
@@ -43,18 +43,17 @@ class  Platform{
             y
          }
         this.width=200
-        this.height=20+0
+        this.height=20
     }
     draw(){
-        c.fillStyle='grey'
+        c.fillStyle='black'
         c.fillRect(this.position.x,this.position.y,this.width,this.height)
     }
 }
-const player = new Player()
+let platforms=[new Platform({x:0,y:470}),new Platform({x:250,y:400}),new Platform({x:600,y:470}),new Platform({x:1000,y:400}),new Platform({x:1400,y:470}),new Platform({x:1750,y:400}),new Platform({x:2050,y:470}),new Platform({x:2550,y:400}),new Platform({x:2850,y:400})]
 
-const platforms=[new Platform({x:0,y:470}),new Platform({x:250,y:400})]
-
-const keys = {
+let player = new Player()
+let keys = {
     right: {
         pressed: false
     },
@@ -64,39 +63,49 @@ const keys = {
 }
  let scrollOffset=0
 
+function init(){
+    platforms=[new Platform({x:0,y:470}),new Platform({x:250,y:400}),new Platform({x:600,y:470}),new Platform({x:1000,y:400}),new Platform({x:1400,y:470}),new Platform({x:1750,y:400}),new Platform({x:2050,y:470}),new Platform({x:2550,y:400}),new Platform({x:2850,y:400})]   
+    player = new Player()
+
+}
 function animate(){
     requestAnimationFrame(animate)
-    c.fillStyle='green'
+    c.fillStyle='skyblue'
     c.fillRect(0,0,canvas.width,canvas.height)
     player.update()
     platforms.forEach(platform=>{
         platform.draw()
     })
     if (keys.right.pressed && player.position.x < 400) {
-        player.velocity.x = 5
+        player.velocity.x = player.speed
     }
     else if (keys.left.pressed && player.position.x >100) {
-        player.velocity.x = -5
+        player.velocity.x = -player.speed
     } 
     else{
         player.velocity.x = 0
         if(keys.right.pressed){
-                scrollOffset += 5
+                scrollOffset += player.speed
                 platforms.forEach((platform)=>{
                     platform.position.x-=5
                 })
             }
             else if(keys.left.presed){
-                scrollOffset -= 5
+                scrollOffset -= player.speed
                 platforms.forEach((platform)=>{
                     platform.position.x+=5
                 })
             }
         } 
 
-    if(scrollOffset>2000){
-        console.log('you win')
+    if(scrollOffset>2600){
+        alert("Game won")
     }
+
+    if(player.position.y>canvas.height){
+        init()
+    }
+
     platforms.forEach(platform=>{
     if(player.position.y+player.height<=platform.position.y && 
         player.position.y+player.height+player.velocity.y>=
@@ -157,3 +166,5 @@ addEventListener('keyup',  ({ keyCode }) => {
 
     console.log(keys.right.pressed)
 })
+
+
